8931dc33851abe4121f2b37703b48ba1fba94c5d
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-09-13 09:52:18 +0100

    Initial Commit

---
+++ MovieTrackingApplication/MovieTrackingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTrackingApplicationApp.swift
+//  MovieTrackingApplication
+//
+//  Created by Clyde Lartey on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieTrackingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
