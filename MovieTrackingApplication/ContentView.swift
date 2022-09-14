22cbddba0dd6ace2fe3e82a9763e0d4bda0a5404
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-09-14 10:52:29 +0100

    Building the homepage

--- MovieTrackingApplication/ContentView.swift
+++ MovieTrackingApplication/ContentView.swift
@@ -17,7 +17,53 @@ struct ContentView: View {
         
         
         VStack {
-            Text("")
+            Text("Homepage")
+                .padding(35)
+                Spacer()
+            
+            
+            HStack {
+                Text("Watchlist Section")
+                    .padding(35)
+                    .foregroundColor(.orange)
+                Spacer()
+                
+            }
+            
+            VStack {
+                Text("Movie 1")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 300, height: 200)
+                    .background(Rectangle().fill(Color.white).shadow(radius: 3))
+
+                
+                Text("Movie 2")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 300, height: 200)
+                    .background(Rectangle().fill(Color.white).shadow(radius: 3))
+
+                
+                Text("Movie 3")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 300, height: 200)
+                    .background(Rectangle().fill(Color.white).shadow(radius: 3))
+
+    
+            }
+            
+            Spacer()
+            Spacer()
+            Spacer()
+             
+                
+            
+                
         }
     }
 }
