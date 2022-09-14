1268bb2edc3b1c4828c2ea48af8eb6f86e64112b
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-09-14 11:10:03 +0100

    Separate pages for the homepage views - thinking of instead just splitting them up and then making them all accessible from the homepage

---
+++ MovieTrackingApplication/RecommendMovies.swift
@@ -0,0 +1,48 @@
+//
+//  RecommendMovies.swift
+//  MovieTrackingApplication
+//
+//  Created by Clyde Lartey on 14/09/2022.
+//
+
+import SwiftUI
+
+struct RecommendMovies: View {
+    var body: some View {
+        VStack {
+            Text("Recommend Movies")
+                .padding(30)
+                Spacer()
+            
+            
+            HStack (spacing:0){
+                Text("Movie 1")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 250, height: 125)
+                    .background(Circle().fill(Color.green).shadow(radius: 3))
+                
+                Text("Movie 1")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 250, height: 125)
+                    .background(Circle().fill(Color.green).shadow(radius: 3))
+                
+                
+                
+            }
+            Spacer()
+            Spacer()
+        }
+        
+     
+}
+
+struct RecommendMovies_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendMovies()
+    }
+}
+}
