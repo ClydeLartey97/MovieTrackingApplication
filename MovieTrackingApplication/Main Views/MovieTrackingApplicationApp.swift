c320c7671ad8b6f2a7ec13187c6543c0823d1488
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-10-04 09:36:06 +0100

    Tab View essentially fully completed

--- MovieTrackingApplication/Main Views/MovieTrackingApplicationApp.swift
+++ MovieTrackingApplication/Main Views/MovieTrackingApplicationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MovieTrackingApplicationApp: App {
     var body: some Scene {
         WindowGroup {
-            HomepageView()
+            MainView()
         }
     }
 }
