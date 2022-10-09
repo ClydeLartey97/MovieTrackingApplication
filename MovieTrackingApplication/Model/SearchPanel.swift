11b375af03c84ec05ac2642ab1770758ed510887
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-10-09 21:56:06 +0100

    Starting to import from API

---
+++ MovieTrackingApplication/Model/SearchPanel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchPanel.swift
+//  MovieTrackingApplication
+//
+//  Created by Clyde Lartey on 09/10/2022.
+//
+
+import Foundation
+
--- MovieTrackingApplication/Preview Content/StateController.swift
+++ MovieTrackingApplication/Preview Content/StateController.swift
@@ -8,21 +8,24 @@
 import Foundation
 
 
-
-func getMovies() {
-    //import the IMAGE from the API
-    //The URL holds the respective URLS for all of the images that i will need
-    guard let url = URL(string:"https://api.themoviedb.org/3/configuration?api_key=<<api_key>>")
-    else{
-        print("URL not valid ")
-        return
-    }
-    
-    let request = URLRequest(url:url)
-    
-    URLSession.shared.dataTask(with: request) { (data, response, error) in
-        if let data = data {
-            print(String(decoding: data, as: UTF8.self))
+class StateController: ObservableObject {
+    func getMovies() {
+        //import the IMAGE from the API
+        //The URL holds the respective URLS for all of the images that i will need
+        guard let url = URL(string:"https://mdblist.p.rapidapi.com/?s=game")
+        else{
+            print("URL not valid ")
+            return
         }
-    }.resume()
+        
+        let request = URLRequest(url:url)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                print(String(decoding: data, as: UTF8.self))
+            }
+        }.resume()
+    }
+
 }
+
